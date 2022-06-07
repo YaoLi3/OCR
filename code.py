bd0141d18f1aaafa0076c539ba66a66bcb357589
YaoLi3 <liyaoo6o@gmail.com>
2022-06-07 09:44:24 +0800

    fix age & dep bugs

--- code.py
+++ code.py
@@ -8,85 +8,16 @@ import pandas as pd
 import os
 import re
 from itertools import product
-
-
-class Entry:
-    def __init__(self, row, col, x, y, text):
-        self.row = row
-        self.col = col
-        self.x = x
-        self.y = y
-        self.text = text
-
-    @property
-    def x(self):
-        return self.x
-
-    @property
-    def y(self):
-        return self.y
-
-    @property
-    def row(self):
-        return self.row
-
-    @property
-    def col(self):
-        return self.col
-
-    @property
-    def text(self):
-        return self.text
-
-
-class Sheet:
-    def __init__(self, table_data) -> None:
-        self.table_data = table_data  # grid
-        # or nested dictionary
-        self.key_value = pd.DataFrame(columns=['k','values'])  # final df to save in an excel
-
-        self.line_data = None
-        self.id = ''  # QDxxxxx
-        # single pages or multi
-        self.single = True     
-        # header
-        # body
-        self.body_coor = [topx,topy,lowx,lowy]
-        # footer
-        
-    def set_qid(self, qid):
-        self.id = qid
-
-    def set_line_data(self, line_data):
-        self.line_data = line_data
-    
-    def save_table(self):
-        self.key_value.to_csv(f'{self.id}.csv', index=False)
-
-    def set_if_single_page(self, d):
-        self.single = if_single_pages(d)
-
-    def get_entry(self, row, col):
-        """when table is a DataFrame"""
-        try:
-            return self.table[self.table['row']==row and self.table['col']==col].values[0]
-        except KeyError:
-            print('this row/col does not exist!')
-
-
-def if_single_pages(d):
-    texts = list(d.keys())
-    if texts.count('姓名') > 1 or texts.count('性别') > 1:
-        return True 
-
-
-def get_value2(table_data:pd.DataFrame, key1, value1, key2):
-    return table_data[table_data[key1]==value1][key2].values[0]
+from collections import namedtuple
 
 
 #####################################################################
 # functions
 ##############################################################################
+def get_value2(table_data:pd.DataFrame, key1, value1, key2):
+    return table_data[table_data[key1]==value1][key2].values[0]
+
+
 def is_value(s,pattern):
     return bool(re.search(pattern, s))
 
@@ -114,20 +45,25 @@ def get_value(s,pattern,x,y,r:list=(-3,3)):
 def get_id(s):
     id_pattern = r'QD[A-Z]+[0-9]+'
     item_id = re.match(id_pattern,s)
-    item_id = item_id.group()
+    #item_id = item_id.group()
     return item_id
 
 
 # 2. TIME----------------------------
 def time_test(s):
     #time_pattern = r'[0-9\-\/\.\:]+'
-    time_pattern = r'[0-9]{4}[\-\/\.\:][0-9\-\/\.\:]+'
+    time_pattern = r'[0-9]{4}[\-\/\.\:][0-9]{1,2}[\-\/\.\:][0-9]{1,2}'
     s = s.split(' ',1)[0]
-    return re.match(time_pattern, s).group()
+    try:
+        return re.search(time_pattern, s).group()
+    except AttributeError:
+        print(s)
+        return 'NOT FOUND'
 
 
 def is_time(s):
-    time_pattern = r'[0-9]{4}[\-\/\.\:][0-9\-\/\.\:]+'
+    #time_pattern = r'[0-9]{4}[\-\/\.\:][0-9\-\/\.\:]+'
+    time_pattern = r'[0-9]{4}[\-\/\.\:][0-9]{1,2}[\-\/\.\:][0-9]{1,2}'
     b = bool(re.search(time_pattern, s))
     return b
 
@@ -140,7 +76,7 @@ def get_time(s,x,y):
         if time_string != '':
             time = time_test(time_string)
         else:
-            for r,c in product(range(-2,3), range(-2,3)):  # TODO: range
+            for r,c in product(range(-4,4), range(-5,7)):  # TODO: range
                 value = table_data[(table_data['row']==y+r)&(table_data['col']==x+c)]
                 if len(value) > 0:
                     text = value['text'].values[0]
@@ -170,7 +106,9 @@ def get_diagnosis(s,x,y):
 def is_age(s):
     if '岁' in s:
         s = s.replace('岁','')
-    age_pattern = r'^[0-9]{2}$'  # TODO: 1. raw string 2. 有没有没有”岁“的
+    elif '日' in s:
+        s = s.replace('日','')
+    age_pattern = r'^[0-9]{1,2}$'  # TODO: 1. raw string 2. 有没有没有”岁“的
     b = bool(re.search(age_pattern, s))
     return b
 
@@ -183,18 +121,15 @@ def is_dob(s):
 
 def get_age(s,x,y):
     age = 'NOT FOUND'
-    age_pattern = r'[0-9]{2}'  # TODO: raw string
+    age_pattern = r'[0-9]{1,2}'  # TODO: raw string
     dob_pattern = r'[0-9]{4}[\/\-][0-9]{1,2}[\/\-][0-9]{1,2}'
     if ':' in s:
         string = s.split(':',1)[-1]
         if string != '':
-            if is_age(string):
-                age = re.match(age_pattern, string).group()  # still contains 岁。。
-                age = age+'岁'
-            elif is_dob(string):
-                age = re.match(dob_pattern, string).group()
+            # 年龄不需要判断
+            age = string
         else:
-            for r,c in product(range(-6,10), range(-6,10)):  # TODO: range
+            for r,c in product(range(-2,3), range(-6,10)):  # TODO: range
                 value = table_data[(table_data['row']==y+r)&(table_data['col']==x+c)]
                 if len(value) > 0:
                     text = value['text'].values[0]
@@ -202,7 +137,10 @@ def get_age(s,x,y):
                         age = re.match(age_pattern, text).group()
                         age = age+'岁'
                     elif is_dob(text):
-                        age = re.match(dob_pattern, text).group()
+                        try:
+                            age = re.match(dob_pattern, text).group()
+                        except AttributeError:
+                            pass
     return age
 
 
@@ -220,10 +158,11 @@ def get_gender(s,x,y):
     if ':' in s:
         string = s.split(':',1)[-1]
         if string != '':
-            if is_gender(string):
-                gender = string
+
+            #if is_gender(string):
+            gender = string
         else:
-            for r,c in product(range(-3,3), range(-3,3)):  # TODO: range
+            for r,c in product(range(-5,5), range(-5,5)):  # TODO: range
                 value = table_data[(table_data['row']==y+r)&(table_data['col']==x+c)]
                 if len(value) > 0:
                     text = value['text'].values[0]
@@ -246,10 +185,11 @@ def get_name(s,x,y):
     if ':' in s:
         string = s.split(':',1)[-1]
         if string != '':
-            if is_name(string):
-                name = re.match(name_pattern, string).group()
+            name = string
+            #if is_name(string):
+            #    name = re.match(name_pattern, string).group()
         else:
-            for r,c in product(range(-3,3), range(-3,3)):  # TODO: range
+            for r,c in product(range(-3,4), range(-5,7)):  # TODO: range
                 value = table_data[(table_data['row']==y+r)&(table_data['col']==x+c)]
                 if len(value) > 0:
                     text = value['text'].values[0]
@@ -272,8 +212,19 @@ def get_dep(s,x,y):
     dep = 'NOT FOUND'
     if ':' in s:
         string = s.split(':',1)[-1]
-        if string != '':           
-            dep = re.match(dep_pattern, string).group()
+        if string != '':
+            if '门诊' in string and len(string) > 2:
+                dep = re.search(dep_pattern, string).group()
+            else:
+                dep = string
+            #if '产科' in string:
+            #    dep = string
+            #else:
+            #    try:
+            #        dep = re.match(dep_pattern, string).group()
+            #    except AttributeError:
+            #        print(s)
+            #        pass
         else:
             for r,c in product(range(-5,5), range(-5,5)):  # TODO: range
                 value = table_data[(table_data['row']==y+r)&(table_data['col']==x+c)]
@@ -286,7 +237,6 @@ def get_dep(s,x,y):
                             dep = re.match(dep_pattern, text).group()
                         except AttributeError:
                             pass
-                    
     return dep
 
 
@@ -406,7 +356,6 @@ def get_target_data(headers,table_data, prefix, proj):
         value = 'NOT FOUND'
 
         # when there's multiple entries meet the condition
-        # TODO: xx
         if len(targets) > 0:  
             target = targets.values[0]
             y = get_value2(table_data, 'text', target, 'row')
@@ -427,7 +376,16 @@ def get_target_data(headers,table_data, prefix, proj):
                         x = get_value2(table_data, 'text', t, 'col')
                         value = get_dep(t,x,y)
             elif key_name == '姓名':
-                value = get_name(target, x, y)
+                value = get_name(target,x,y)
+                #for t in targets:
+                #    y = get_value2(table_data, 'text', t, 'row')
+                #    x = get_value2(table_data, 'text', t, 'col')
+                #    value = get_name(t, x, y)
+                    #if value == 'NOT FOUND':
+                    #    try:
+                    #        value = re.search(u"^[\u4e00-\u9fa5]{2,3}$", prefix).group()
+                    #    except AttributeError:
+                    #        value = 'NOT FOUND'
             elif key_name == '临床诊断':
                 value = get_diagnosis(target, x, y)
             else:
@@ -441,7 +399,6 @@ def get_target_data(headers,table_data, prefix, proj):
                         value = value['text'].iloc[0]
                     except IndexError:
                         value = 'NOT FOUND'
-                        
 
         # fixed values
         if key_name == '全人群系统项目编号':
@@ -454,80 +411,17 @@ def get_target_data(headers,table_data, prefix, proj):
     return kvalue
 
 
-def find_section(ts):
-    topx = 0; topy = 0; lowx = 0; lowy = 0
-    sub_topxs = []; sub_topys = []; sub_lowxs = []; sub_lowys = []
-    for t in ts:
-        if t.text in ['参考区间', '参考值', '单位']:
-            sub_topxs.append(t.x)
-            sub_topys.append(t.y)
-            topx = t.x+1; topy = t.y+1
-        if "备注" in t.text or '检验者' in  t.text or '检验医' in t.text:
-            sub_lowxs.append(t.x)
-            sub_lowys.append(t.y)
-            lowx = t.x-1; lowy = t.y-1
-    #topx = sorted(sub_topxs, reverse=True)[0]
-    #topy = sorted(sub_topys, reverse=True)[0]
-    #lowx = sorted(sub_lowxs)[0]
-    #lowy = sorted(sub_lowys)[0]
-    return topx,topy,lowx,lowy
-
-
-def sort_section_col(ts, topy, lowy): # sort by x value
-    sub_ts = []
-    for t in ts:
-        if t.y < lowy and t.y > topy and not re.search(r"备注", t.text):
-            #if not re.search(r"备注", t.text):
-            sub_ts.append(t)
-    sss = sorted(sub_ts, key=lambda x:x.x)
-    return sss
-
-
-def detect_col(sss, rcor=7, ccor=10):
-    test = {}
-    n = 1
-    test[n] = []
-    for i in range(1, len(sss)):
-        start_y = sss[0].y
-        ct = sss[i]; lt = sss[i-1]
-        print(lt.text, lt.row, lt.y)
-        test[n].append(lt)
-        x_change = ct.x - lt.x
-        y_change = ct.y - start_y
-        if x_change > row_inter*rcor and y_change < col_inter*ccor:
-            print('----------')
-            n += 1
-            test[n] = []
-    return test
-
-
-def match_row(test, range_cor=2):
-    data = {}
-    v1 = sorted(test[1], key=lambda x: x.y)
-    v3 = sorted(test[3], key=lambda x: x.y)
-    for i in v1:
-        for j in v3:
-            if abs(i.y-j.y) <= row_inter*range_cor:
-                #print(i.text, j.text)
-                data[i.text] = j.text
-            #if abs(i.row-j.row) == 1:
-                #print(i.text, j.text)
-    return data
-
-
-def get_value(l3, sub_name):
-    for t,v in l3.items():
-        if t in sub_name:
-            return v
-        
-
-
 if __name__ == '__main__':
     imagejson_filename = sys.argv[1]
-    name = get_id(os.path.basename(imagejson_filename))
     headerjson_filename = sys.argv[2]
     prefix = sys.argv[3]
     proj=sys.argv[4]
+    name = get_id(os.path.basename(imagejson_filename))
+    if name is None:
+        name = prefix
+    else:
+        name=name.group()
+
 
     # loading image json
     image_data, row_inter, col_inter = loading_json_content(imagejson_filename)
@@ -535,23 +429,6 @@ if __name__ == '__main__':
     col_data = get_column_ids(image_data,col_inter)
     row_data = get_row_ids(image_data,row_inter)
     table_data = merge_row_col(col_data,row_data)
-
-    item_id = imagejson_filename.split("/")[-1]
-    id_pattern = r'QD[A-Z]+[0-9]+'
-    item_id = re.match(id_pattern,item_id)
-    item_id = item_id.group()
-    old_row = 0
-    line_data = []
-    with open(f"{prefix}.table.txt", 'w') as f:
-        for i,line in table_data.iterrows():
-            if old_row == line['row']:
-                index_col = line['col']
-                line_data.append(line['text'])
-            else:
-                f.writelines(f'{line_data}\n')
-                old_row += 1
-                line_data = []
-                line_data.append(line['text'])
     table_data.to_csv(f"{prefix}.table.csv",encoding='utf_8_sig',header=True,index=False)
 
 
@@ -563,26 +440,6 @@ if __name__ == '__main__':
     ###########
     # save csv
     kvalue = kvalue.T
-    print(kvalue)
     kvalue.to_csv(f"{prefix}.csv",encoding='utf_8_sig',header=False)
 
-    # ts = []
-    # for text in table_data['text']:
-    #     t = Entry(table_data[table_data['text']==text]['row'].values[0], \
-    #         table_data[table_data['text']==text]['col'].values[0], \
-    #             table_data[table_data['text']==text]['x'].values[0], \
-    #                 table_data[table_data['text']==text]['y'].values[0], \
-    #                     text)
-    #     ts.append(t)
-
-
-    # ts = sorted(ts, key=lambda x: (x.x, x.y))
-    # x1,y1,x2,y2 = find_section(ts)
-    # l1 = sort_section_col(ts, y1, y2)
-    # l2 = detect_col(l1)
-    # l3 = match_row(l2)
-    # for kp in headers:
-    #     v = get_value(l3, kp[1])
-    #     if v is not None:
-    #         print(f'{kp[0]}\t{v}')
 
